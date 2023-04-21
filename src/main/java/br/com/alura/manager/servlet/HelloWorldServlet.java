9eaab16c347e47da1eeeae8f207022c3cc89d179
Pedro C. Pacheco <cpedropacheco@hotmail.com>
2023-04-21 12:26:15 -0300

    feat: creating a response to the servlet

--- src/main/java/br/com/alura/manager/servlet/HelloWorldServlet.java
+++ src/main/java/br/com/alura/manager/servlet/HelloWorldServlet.java
@@ -1,16 +1,29 @@
 package br.com.alura.manager.servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+// To create the path to the Servlet:
+//@WebServlet is a anotation, and the urlPatterns is the url
+
+@WebServlet(urlPatterns = "/hello") 
 public class HelloWorldServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
 	
-	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) {
-
+	@Override // HTTP Protocol always have a Request and a Response
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Congrats, this is a Servlet!</h1>");
+		out.println("</body>");
+		out.println("</html>");
 	}
 	
 
