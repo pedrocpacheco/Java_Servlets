50df55bdefe7b13c636312623029175c79c38f59
Pedro C. Pacheco <cpedropacheco@hotmail.com>
2023-04-21 12:19:14 -0300

    feat(JavaResources): creating a HttpServlet Extended Class

---
+++ src/main/java/br/com/alura/manager/servlet/HelloWorldServlet.java
@@ -0,0 +1,17 @@
+package br.com.alura.manager.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) {
+
+	}
+	
+
+}
